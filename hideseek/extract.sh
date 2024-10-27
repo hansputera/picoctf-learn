025023a0e5e61da2e0b0b1a80d84237867cf65a8
hansputera <hanifdwyputrasembiring@gmail.com>
2024-10-27 12:32:02 +0800

    push
    Signed-off-by: hansputera <hanifdwyputrasembiring@gmail.com>

---
+++ hideseek/extract.sh
@@ -0,0 +1,1 @@
+vim ukn_reality.jpg
---
+++ webdecode/decode.js
@@ -0,0 +1,3 @@
+const key = 'cGljb0NURnt3ZWJfc3VjYzNzc2Z1bGx5X2QzYzBkZWRfMjgzZTYyZmV9'; // /about.html
+
+console.log(Buffer.from(key, 'base64').toString('utf8'));
